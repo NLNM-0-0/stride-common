a7bb385ec3466b850bc85fc6f8f9b79a549718d3
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-04-06 22:52:42 +0700

    fix: use generation type UUID for JpaBaseEntity

--- src/main/java/com/stride/tracking/commons/persistence/jpa/JpaBaseEntity.java
+++ src/main/java/com/stride/tracking/commons/persistence/jpa/JpaBaseEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Setter
 public abstract class JpaBaseEntity extends JpaAuditEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
     private Long id;
 }

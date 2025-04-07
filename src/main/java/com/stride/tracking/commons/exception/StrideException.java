9b41dc3ed69410a88430f21a35ab26db21e11abc
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-04-07 07:58:35 +0700

    fix: StrideException

--- src/main/java/com/stride/tracking/commons/exception/StrideException.java
+++ src/main/java/com/stride/tracking/commons/exception/StrideException.java
@@ -1,12 +1,16 @@
 package com.stride.tracking.commons.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@RequiredArgsConstructor
 @Getter
 public class StrideException extends RuntimeException {
 	private final HttpStatus status;
 	private final String message;
+
+	public StrideException(HttpStatus status, String message) {
+		super(message);
+		this.message = message;
+		this.status = status;
+	}
 }

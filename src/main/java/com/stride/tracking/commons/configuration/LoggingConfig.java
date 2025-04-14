5ef285803fe37965fdc3a1a2cc4ee189d2a79e78
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-04-14 10:13:23 +0700

    fix: put X-Request-ID và X-Auth-User-Id to MDC

--- src/main/java/com/stride/tracking/commons/configuration/LoggingConfig.java
+++ src/main/java/com/stride/tracking/commons/configuration/LoggingConfig.java
@@ -5,12 +5,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -21,13 +21,19 @@ public class LoggingConfig extends OncePerRequestFilter {
             HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        Optional<String> requestId = Optional.ofNullable(request.getHeader(CustomHeaders.X_REQUEST_ID));
-
-        if (requestId.isEmpty() || !requestId.get().isBlank()) {
-            requestId = Optional.of(UUID.randomUUID().toString());
+        String requestId = request.getHeader(CustomHeaders.X_REQUEST_ID);
+        if (requestId == null || requestId.isBlank()) {
+            requestId = UUID.randomUUID().toString();
         }
 
-        response.addHeader(CustomHeaders.X_REQUEST_ID, requestId.get());
+        response.addHeader(CustomHeaders.X_REQUEST_ID, requestId);
+
+        MDC.put(CustomHeaders.X_REQUEST_ID, requestId);
+
+        String userId = request.getHeader(CustomHeaders.X_AUTH_USER_ID);
+        if (userId != null && !userId.isBlank()) {
+            MDC.put(CustomHeaders.X_AUTH_USER_ID, userId);
+        }
 
         filterChain.doFilter(request, response);
     }

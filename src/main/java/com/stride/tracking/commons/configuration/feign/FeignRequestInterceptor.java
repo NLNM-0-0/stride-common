b8a0765e7705d8a0b5c8767d9ab8a4de328abf3e
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-04-14 16:36:40 +0700

    update: get info from UserDetails and request header

--- src/main/java/com/stride/tracking/commons/configuration/feign/FeignRequestInterceptor.java
+++ src/main/java/com/stride/tracking/commons/configuration/feign/FeignRequestInterceptor.java
@@ -1,27 +1,21 @@
 package com.stride.tracking.commons.configuration.feign;
 
 import com.stride.tracking.commons.constants.CustomHeaders;
+import com.stride.tracking.commons.utils.HeaderUtils;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 public class FeignRequestInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate template) {
-        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if (requestAttributes != null) {
-            var currentRequest = requestAttributes.getRequest();
-
-            template.header(CustomHeaders.X_AUTH_USER_ID, currentRequest.getHeader(CustomHeaders.X_AUTH_USER_ID));
-            template.header(CustomHeaders.X_AUTH_USERNAME, currentRequest.getHeader(CustomHeaders.X_AUTH_USERNAME));
-            template.header(CustomHeaders.X_AUTH_EMAIL, currentRequest.getHeader(CustomHeaders.X_AUTH_EMAIL));
-            template.header(CustomHeaders.X_AUTH_PROVIDER, currentRequest.getHeader(CustomHeaders.X_AUTH_PROVIDER));
-            template.header(CustomHeaders.X_AUTH_USER_AUTHORITIES, currentRequest.getHeader(CustomHeaders.X_AUTH_USER_AUTHORITIES));
-            template.header(CustomHeaders.X_REQUEST_ID, currentRequest.getHeader(CustomHeaders.X_REQUEST_ID));
-        }
+        Map<String, String> headers = HeaderUtils.getHeaders();
+        headers.forEach(template::header);
     }
 }
---
+++ src/main/java/com/stride/tracking/commons/utils/HeaderUtils.java
@@ -0,0 +1,67 @@
+package com.stride.tracking.commons.utils;
+
+import com.stride.tracking.commons.configuration.security.UserDetailsImpl;
+import com.stride.tracking.commons.constants.CustomHeaders;
+import com.stride.tracking.commons.constants.StrideConstants;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HeaderUtils {
+    private HeaderUtils() {
+    }
+
+    public static Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+
+        putAuthInfo(headers);
+
+        putRequestId(headers);
+
+        return headers;
+    }
+
+    private static void putAuthInfo(Map<String, String> headers) {
+        Authentication authentication = SecurityUtils.getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+            headers.put(CustomHeaders.X_AUTH_USER_ID, userDetails.getId());
+            headers.put(CustomHeaders.X_AUTH_EMAIL, userDetails.getEmail());
+            headers.put(CustomHeaders.X_AUTH_PROVIDER, userDetails.getProvider());
+            headers.put(CustomHeaders.X_AUTH_USERNAME, userDetails.getUsername());
+            headers.put(
+                    CustomHeaders.X_AUTH_USER_AUTHORITIES,
+                    userDetails.getAuthorities().stream()
+                            .map(GrantedAuthority::getAuthority)
+                            .collect(Collectors.joining(StrideConstants.DELIMITER_COMMA))
+            );
+        }
+    }
+
+    private static void putRequestId(Map<String, String> headers) {
+        HttpServletRequest request = getCurrentHttpRequest();
+        if (request != null) {
+            String requestId = request.getHeader(CustomHeaders.X_REQUEST_ID);
+            if (requestId != null) {
+                headers.put(CustomHeaders.X_REQUEST_ID, requestId);
+            }
+        }
+    }
+
+    private static HttpServletRequest getCurrentHttpRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes instanceof ServletRequestAttributes servletRequestAttributes) {
+            return servletRequestAttributes.getRequest();
+        }
+        return null;
+    }
+}

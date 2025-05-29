d4d43b973fc8c7207e152cc20de98c7e959a8401
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-29 22:15:20 +0700

    dev: add preAuthorize feature

---
+++ src/main/java/com/stride/tracking/commons/annotations/PreAuthorizeUser.java
@@ -0,0 +1,14 @@
+package com.stride.tracking.commons.annotations;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority(T(com.stride.tracking.commons.constants.AuthorityConstants).USER)")
+public @interface PreAuthorizeUser {
+}

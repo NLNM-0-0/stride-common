eecfce086033aa951706e26e3c12e823ca2ba272
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-29 22:01:02 +0700

    dev: add AuthorityConstants

---
+++ src/main/java/com/stride/tracking/commons/constants/AuthorityConstants.java
@@ -0,0 +1,9 @@
+package com.stride.tracking.commons.constants;
+
+public class AuthorityConstants {
+    private AuthorityConstants() {
+    }
+
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+}

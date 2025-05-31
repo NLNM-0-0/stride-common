1d8e21b3910e2eb3b9c506bcd79599817b8cfcde
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-31 23:56:42 +0700

    dev: add some Kafka topics

--- src/main/java/com/stride/tracking/commons/constants/KafkaTopics.java
+++ src/main/java/com/stride/tracking/commons/constants/KafkaTopics.java
@@ -5,4 +5,7 @@ public class KafkaTopics {
 
     public static final String NOTIFICATION_TOPIC = "notification_delivery";
     public static final String FCM_TOPIC = "fcm_delivery";
+    public static final String ACTIVITY_CREATED_TOPIC = "activity_created";
+    public static final String SPORT_UPDATED_TOPIC = "sport_updated";
+    public static final String USER_CREATED_TOPIC = "user_created";
 }

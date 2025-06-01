b008e4937391adf965370ed7571ea0fef7acf4bb
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-02 05:46:03 +0700

    deV: add ACTIVITY_UPDATED_TOPIC

--- src/main/java/com/stride/tracking/commons/constants/KafkaTopics.java
+++ src/main/java/com/stride/tracking/commons/constants/KafkaTopics.java
@@ -7,6 +7,7 @@ public class KafkaTopics {
     public static final String FCM_TOPIC = "fcm_delivery";
 
     public static final String ACTIVITY_CREATED_TOPIC = "activity_created";
+    public static final String ACTIVITY_UPDATED_TOPIC = "activity_updated";
     public static final String ACTIVITY_DELETED_TOPIC = "activity_deleted";
 
     public static final String SPORT_CREATED_TOPIC = "sport_created";

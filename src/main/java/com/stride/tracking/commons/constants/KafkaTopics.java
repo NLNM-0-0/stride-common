7c94777a70276152527812a385840b47684beb06
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-06-07 11:16:19 +0700

    dev: add FCM_DELETED_TOPIC

--- src/main/java/com/stride/tracking/commons/constants/KafkaTopics.java
+++ src/main/java/com/stride/tracking/commons/constants/KafkaTopics.java
@@ -5,6 +5,7 @@ public class KafkaTopics {
 
     public static final String NOTIFICATION_TOPIC = "notification_delivery";
     public static final String FCM_TOPIC = "fcm_delivery";
+    public static final String FCM_DELETED_TOPIC = "fcm_deleted_delivery";
 
     public static final String ACTIVITY_CREATED_TOPIC = "activity_created";
     public static final String ACTIVITY_UPDATED_TOPIC = "activity_updated";
@@ -17,4 +18,6 @@ public class KafkaTopics {
     public static final String SPORT_UPDATED_TOPIC = "sport_updated";
 
     public static final String USER_CREATED_TOPIC = "user_created";
+
+    public static final String DELETE_FCM_TOKEN_TOPIC = "delete_fcm_token";
 }
